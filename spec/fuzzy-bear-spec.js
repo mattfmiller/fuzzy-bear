95751a2c99b88d93aa9aedfc1b26755b48468f8e
Mike Chu and Matt Miller <mike@>
2018-04-23 09:11:20 -0700

    Complete fuzzy bear tutorial.

---
+++ spec/fuzzy-bear-spec.js
@@ -0,0 +1,41 @@
+import { bear } from './../src/fuzzy-bear.js';
+
+describe('HungryBear', function() {
+  let fuzzy = bear;
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    fuzzy.foodLevel = 10;
+    fuzzy.name = "Fuzzy";
+    fuzzy.setHunger();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('should have a name and a food level of 10 when it is created', function() {
+    expect(fuzzy.name).toEqual("Fuzzy");
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+
+  it('should have a food level of 7 after 3001 milliseconds', function() {
+    jasmine.clock().tick(3001);
+    expect(fuzzy.foodLevel).toEqual(7);
+  });
+
+  it('should get very hungry if the food level drops below zero', function() {
+    fuzzy.foodLevel = 0;
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should get very hungry if 10 seconds pass without feeding', function() {
+    jasmine.clock().tick(10001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should return that the bear ate blueberries and the food level should go up 5', function() {
+    expect(fuzzy.eatSmall("blueberries")).toEqual("The bear ate the blueberries! Food level goes up 5!");
+    expect(fuzzy.foodLevel).toEqual(15);
+  });
+});
---
+++ src/fuzzy-bear.js
@@ -0,0 +1,36 @@
+let bear = {
+  foodLevel: 10,
+  setHunger: function() {
+    const hungerInterval = setInterval(() => {
+      this.foodLevel--;
+      if (this.didYouGetEaten() == true) {
+        clearInterval(hungerInterval);
+        return "You got eaten!";
+      }
+    }, 1000);
+  },
+  didYouGetEaten: function() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  feed: function(amount) {
+    let that = this;
+    return function(food) {
+      that.foodLevel += amount
+      return `The bear ate the ${food}! Food level goes up ${amount}!`
+    }
+  }
+};
+
+bear.eatSmall = bear.feed(5);
+bear.eatMedium = bear.feed(10);
+bear.eatLarge = bear.feed(15);
+bear.eatYuck = bear.feed(-10);
+bear.eatPowerUp = bear.feed(50);
+bear.eatSpecialBonus = bear.feed(100);
+bear.eatWeirdThing = bear.feed(Math.floor((Math.random() * 20) + 1));
+
+export { bear };
